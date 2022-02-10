550fb6cbd9c9b857fec9082f79615e8ee9017a8c
HamzaFudda <humzafudda31@gmail.com>
2022-02-11 03:22:08 +0500

    1

---
+++ lib/dishes.dart
@@ -0,0 +1,8 @@
+//class to use dish as an object to streamline our code
+class dish {
+  final String name;
+  final String description;
+  final double price;
+
+  dish({required this.name, required this.description, required this.price});
+}
